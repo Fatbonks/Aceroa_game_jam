ea464a897bc16811cfaed7835d8c13b4ea1eec2a
Fatbonks <106663968+Fatbonks@users.noreply.github.com>
2024-03-06 11:45:06 +1300

    added exit jumps

--- objects/Player/player.gd
+++ objects/Player/player.gd
@@ -4,14 +4,16 @@ extends CharacterBody3D
 @onready var head:Node3D = %head
 
 @export var mouse_sen: float = 0.5
+@export var max_jump_amount: int = 1
+var jump_amount: int
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	jump_amount = max_jump_amount
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -24,10 +26,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	else:
+		jump_amount = max_jump_amount
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_amount > 0:
 		velocity.y = JUMP_VELOCITY
+		jump_amount -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -39,5 +44,4 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
